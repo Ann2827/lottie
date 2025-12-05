0f122573098e46003c9ef82b762b8c43177fc32c
Bystrova Ann <ann.bystrova96@mail.ru>
2025-12-05 20:20:22 +0300

    feat: add unauthorized cat

--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,25 @@ if (elementDark) {
     path: "animation-dark.json", // the path to the animation json
   });
 }
+
+const elementUnauthorized = document.querySelector("#lottie-container-unauthorized");
+if (elementUnauthorized) {
+  lottie.loadAnimation({
+    container: elementUnauthorized, // the dom element that will contain the animation
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "unauthorized.json", // the path to the animation json
+  });
+}
+
+const elementUnauthorizedColored = document.querySelector("#lottie-container-unauthorized-colored");
+if (elementUnauthorizedColored) {
+  lottie.loadAnimation({
+    container: elementUnauthorizedColored, // the dom element that will contain the animation
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "unauthorizedColored.json", // the path to the animation json
+  });
+}
