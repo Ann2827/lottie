1a96dd150742f88a6ae5c5a94041fc49276ab312
Bystrova Ann <ann.bystrova96@mail.ru>
2026-04-20 13:45:42 +0300

    feat: add 404 default

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
 import "./style.css";
 import lottie from "lottie-web";
 
-const elementLight = document.querySelector("#lottie-container-light");
-if (elementLight) {
-  const animation = lottie.loadAnimation({
-    container: elementLight, // the dom element that will contain the animation
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: "animation-light.json", // the path to the animation json
-  });
-}
-
 const elementDark = document.querySelector("#lottie-container-dark");
 if (elementDark) {
   lottie.loadAnimation({
@@ -23,17 +12,6 @@ if (elementDark) {
   });
 }
 
-const elementUnauthorized = document.querySelector("#lottie-container-unauthorized");
-if (elementUnauthorized) {
-  lottie.loadAnimation({
-    container: elementUnauthorized, // the dom element that will contain the animation
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: "unauthorized.json", // the path to the animation json
-  });
-}
-
 const elementUnauthorizedColored = document.querySelector("#lottie-container-unauthorized-colored");
 if (elementUnauthorizedColored) {
   lottie.loadAnimation({
@@ -42,5 +20,48 @@ if (elementUnauthorizedColored) {
     loop: true,
     autoplay: true,
     path: "unauthorizedColored.json", // the path to the animation json
+  });
+}
+
+const elementSleep = document.querySelector("#lottie-container-sleep");
+if (elementSleep) {
+  lottie.loadAnimation({
+    container: elementSleep, // the dom element that will contain the animation
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "sleep.json", // the path to the animation json
+  });
+}
+const elementSleep2 = document.querySelector("#lottie-container-sleep2");
+if (elementSleep2) {
+  lottie.loadAnimation({
+    container: elementSleep2, // the dom element that will contain the animation
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "sleep2.json", // the path to the animation json
+  });
+}
+
+const elementOne = document.querySelector("#lottie-container-one");
+if (elementOne) {
+  lottie.loadAnimation({
+    container: elementOne, // the dom element that will contain the animation
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "one.json", // the path to the animation json
+  });
+}
+
+const elementHm = document.querySelector("#lottie-container-hm");
+if (elementHm) {
+  lottie.loadAnimation({
+    container: elementHm, // the dom element that will contain the animation
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "hm.json", // the path to the animation json
   });
 }
